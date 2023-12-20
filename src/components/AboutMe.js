87db065a3ee5e36570b0d8d9f3c742e85a419e52
Serap <77828715+serapo@users.noreply.github.com>
2023-12-20 10:52:24 +0300

    icon style changed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,20 +6,34 @@ import Icon from "@mdi/react";
 import { mdiGithub } from "@mdi/js";
 import { mdiLinkedin } from "@mdi/js";
 import { mdiGitlab } from "@mdi/js";
+import React, { useState } from "react";
 
 function AboutMe() {
+  const [hoveredIcon, setHoveredIcon] = useState(null);
   const openLink = (url) => {
     window.open(url, "_blank");
+  };
+  const iconStyle = {
+    fill: "white",
+    width: "24px",
+    height: "24px",
+    cursor: "pointer",
+  };
+
+  const handleIconHover = (iconName) => {
+    setHoveredIcon(iconName);
+  };
+
+  const handleIconLeave = () => {
+    setHoveredIcon(null);
   };
 
   return (
     <Box
       sx={{
         textAlign: "center",
-
         padding: 0,
         margin: 0,
-
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
@@ -60,16 +74,34 @@ function AboutMe() {
             <Icon
               path={mdiGitlab}
               size={2}
+              style={{
+                ...iconStyle,
+                color: hoveredIcon === "gitlab" ? "orange" : "white",
+              }}
+              onMouseEnter={() => handleIconHover("gitlab")}
+              onMouseLeave={handleIconLeave}
               onClick={() => openLink("https://gitlab.com/serapo")}
             />
             <Icon
               path={mdiLinkedin}
+              style={{
+                ...iconStyle,
+                color: hoveredIcon === "linkedin" ? "orange" : "white",
+              }}
+              onMouseEnter={() => handleIconHover("linkedin")}
+              onMouseLeave={handleIconLeave}
               size={2}
               onClick={() => openLink("https://www.linkedin.com/in/serapo/")}
             />
             <Icon
               path={mdiGithub}
               size={2}
+              style={{
+                ...iconStyle,
+                color: hoveredIcon === "github" ? "orange" : "white",
+              }}
+              onMouseEnter={() => handleIconHover("github")}
+              onMouseLeave={handleIconLeave}
               onClick={() => openLink("https://github.com/serapo")}
             />
           </Box>
