fe6768db80c55e407f78593be2075954f467ec8d
Serap <77828715+serapo@users.noreply.github.com>
2023-12-20 13:36:28 +0300

    Skills.js updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -41,7 +41,7 @@ function Skills() {
         width: "50%",
       }}
     >
-      <Typography variant="h3" style={{ color: "white" }}>
+      <Typography variant="h4" style={{ color: "white" }}>
         SKILLS
         <Divider
           sx={{
@@ -52,6 +52,301 @@ function Skills() {
           }}
         />
       </Typography>
+      <Box
+        sx={{
+          textAlign: "center",
+          padding: 0,
+          margin: 0,
+          marginBottom: 10,
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: 2,
+          color: "white",
+          width: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "HTML5" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("HTML5")}
+            onMouseLeave={handleIconLeave}
+            path={mdiLanguageHtml5}
+            size={3}
+          />
+          <Typography variant="h6" style={{ color: "white", marginTop: 8 }}>
+            HTML5
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "CSS3" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("CSS3")}
+            onMouseLeave={handleIconLeave}
+            path={mdiLanguageCss3}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            CSS
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "JAVASCRIPT" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("JAVASCRIPT")}
+            onMouseLeave={handleIconLeave}
+            path={mdiLanguageJavascript}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            JAVASCRIPT + ES6
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "VUE" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("VUE")}
+            onMouseLeave={handleIconLeave}
+            path={mdiVuejs}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            VUE
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          textAlign: "center",
+          padding: 0,
+          margin: 0,
+          marginBottom: 10,
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: 2,
+          color: "white",
+          width: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "VUETIFY" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("VUETIFY")}
+            onMouseLeave={handleIconLeave}
+            path={mdiVuetify}
+            size={3}
+          />
+          <Typography variant="h6" style={{ color: "white", marginTop: 8 }}>
+            VUETIFY
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "BOOTSTRAP" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("BOOTSTRAP")}
+            onMouseLeave={handleIconLeave}
+            path={mdiBootstrap}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            BOOTSTRAP
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "MATERIALUI" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("MATERIALUI")}
+            onMouseLeave={handleIconLeave}
+            path={mdiMaterialUi}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            MATERIAL UI
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "GIT" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("GIT")}
+            onMouseLeave={handleIconLeave}
+            path={mdiGit}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            GIT
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          textAlign: "center",
+          padding: 0,
+          margin: 0,
+          marginBottom: 10,
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: 2,
+          color: "white",
+          width: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "JIRA" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("JIRA")}
+            onMouseLeave={handleIconLeave}
+            path={mdiJira}
+            size={3}
+          />
+          <Typography variant="h6" style={{ color: "white", marginTop: 8 }}>
+            JIRA
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "REACT" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("REACT")}
+            onMouseLeave={handleIconLeave}
+            path={mdiReact}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            REACT
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "PYTHON" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("PYTHON")}
+            onMouseLeave={handleIconLeave}
+            path={mdiLanguagePython}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            PYTHON
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            style={{
+              ...iconStyle,
+              color: hoveredIcon === "SASS" ? "orange" : "white",
+            }}
+            onMouseEnter={() => handleIconHover("SASS")}
+            onMouseLeave={handleIconLeave}
+            path={mdiSass}
+            size={3}
+          />
+          <Typography variant="h6  " style={{ color: "white", marginTop: 8 }}>
+            SASS
+          </Typography>
+        </Box>
+      </Box>
 
       <Box
         sx={{
@@ -65,154 +360,8 @@ function Skills() {
           color: "white",
           width: "100%",
         }}
-      >
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "HTML5" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("HTML5")}
-          onMouseLeave={handleIconLeave}
-          path={mdiLanguageHtml5}
-          size={3}
-        />
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "CSS3" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("CSS3")}
-          onMouseLeave={handleIconLeave}
-          path={mdiLanguageCss3}
-          size={3}
-        />
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "JAVASCRIPT" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("JAVASCRIPT")}
-          onMouseLeave={handleIconLeave}
-          path={mdiLanguageJavascript}
-          size={3}
-        />
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "VUE" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("VUE")}
-          onMouseLeave={handleIconLeave}
-          path={mdiVuejs}
-          size={3}
-        />
-      </Box>
-      <Box
-        sx={{
-          textAlign: "center",
-          padding: 0,
-          margin: 0,
-          marginBottom: 10,
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: 2,
-          color: "white",
-          width: "100%",
-        }}
-      >
-        <Typography variant="H4" style={{ color: "white" }}>
-          HTML5
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          CSS
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          JAVASCRIPT
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          VUE
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          textAlign: "center",
-          padding: 0,
-          margin: 0,
-          marginBottom: 0,
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: 2,
-          color: "white",
-          width: "100%",
-        }}
-      >
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "VUETIFY" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("VUETIFY")}
-          onMouseLeave={handleIconLeave}
-          path={mdiVuetify}
-          size={3}
-        />
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "BOOTSTRAP" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("BOOTSTRAP")}
-          onMouseLeave={handleIconLeave}
-          path={mdiBootstrap}
-          size={3}
-        />
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "MATERIALUI" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("MATERIALUI")}
-          onMouseLeave={handleIconLeave}
-          path={mdiMaterialUi}
-          size={3}
-        />
-        <Icon
-          style={{
-            ...iconStyle,
-            color: hoveredIcon === "GIT" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("GIT")}
-          onMouseLeave={handleIconLeave}
-          path={mdiGit}
-          size={3}
-        />
-      </Box>
-      <Box
-        sx={{
-          textAlign: "center",
-          padding: 0,
-          margin: 0,
-          marginBottom: 10,
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: 2,
-          color: "white",
-          width: "100%",
-        }}
-      >
-        <Typography variant="H4" style={{ color: "white" }}>
-          VUETIFY
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          BOOTSTRAP
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          Material UI
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          Git
-        </Typography>
-      </Box>
+      ></Box>
+
       <Box
         sx={{
           textAlign: "center",
@@ -224,58 +373,7 @@ function Skills() {
           color: "white",
           width: "100%",
         }}
-      >
-        <Icon style={{
-            ...iconStyle,
-            color: hoveredIcon === "JIRA" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("JIRA")}
-          onMouseLeave={handleIconLeave} path={mdiJira} size={3} />
-        <Icon style={{
-            ...iconStyle,
-            color: hoveredIcon === "REACT" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("REACT")}
-          onMouseLeave={handleIconLeave}  path={mdiReact} size={3} />
-        <Icon  style={{
-            ...iconStyle,
-            color: hoveredIcon === "PYTHON" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("PYTHON")}
-          onMouseLeave={handleIconLeave} path={mdiLanguagePython} size={3} />
-        <Icon style={{
-            ...iconStyle,
-            color: hoveredIcon === "SASS" ? "orange" : "white",
-          }}
-          onMouseEnter={() => handleIconHover("SASS")}
-          onMouseLeave={handleIconLeave} path={mdiSass} size={3} />
-      </Box>
-      <Box
-        sx={{
-          textAlign: "center",
-          padding: 0,
-          margin: 0,
-          marginBottom: 0,
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: 2,
-          color: "white",
-          width: "100%",
-        }}
-      >
-        <Typography variant="H4" style={{ color: "white" }}>
-          JIRA
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          REACT
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          PYTHON
-        </Typography>
-        <Typography variant="H4" style={{ color: "white" }}>
-          SASS
-        </Typography>
-      </Box>
+      ></Box>
     </Box>
   );
 }
