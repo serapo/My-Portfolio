8fa9198b1f71ea07018ea335bc58b5e87e260064
serap <serapp.oral@gmail.com>
2023-12-22 18:57:35 +0300

    CurrentProjects updated

--- src/components/CurrentProjects.js
+++ src/components/CurrentProjects.js
@@ -2,6 +2,8 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+import PmpPlusImage from "../assets/pmpplus.png";
+import Grid from "@mui/material/Grid";
 function CurrentProjects() {
   return (
     <Box
@@ -20,11 +22,35 @@ function CurrentProjects() {
           sx={{
             height: 4,
             backgroundColor: "orange",
-            width: "45%",
+            width: "50%",
             margin: "auto",
           }}
         />
       </Typography>
+
+      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+        <img
+          src={PmpPlusImage}
+          alt="PmpPlus"
+          width="150"
+          height="150"
+          style={{ borderRadius: "8px" }}
+        />
+        <Box sx={{ marginLeft: 2, textAlign: "left", width: 500 }}>
+          <Typography
+            variant="h6"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 2,
+            }}
+          >
+            It is a type of software system that helps organizations automate
+            and manage core business processes for optimal performance.
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 }
