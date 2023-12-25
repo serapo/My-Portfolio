a24a72a008ce263160ab218e037d96c7202d53e7
Serap <77828715+serapo@users.noreply.github.com>
2023-12-25 09:29:05 +0300

    Project.js updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,23 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import React from "react";
+import PmpPlusImage from "../assets/pmpplus.png";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 function Projects() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleTabClick = () => {
+    window.location.href = 'https://serapo.github.io/005-Weather-App/';
+  };
+  const handleTabClickGithub = () => {
+    window.location.href = 'https://github.com/serapo/005-Weather-App?tab=readme-ov-file';
+  };
   return (
     <Box
       sx={{
@@ -29,21 +44,47 @@ function Projects() {
 
       <Box
         sx={{
-          textAlign: "center",
-          padding: 0,
-          margin: 0,
-          marginBottom: 10,
           display: "flex",
-          justifyContent: "space-between",
-          marginTop: 2,
-          color: "white",
-          width: "100%",
+          alignItems: "flex-start",
+          marginBottom: 8,
+          marginTop: 6,
         }}
       >
-       
-        <Typography variant="h6" style={{ color: "white", marginTop: 8 }}>
-          HTML5
-        </Typography>
+        <img
+          src={PmpPlusImage}
+          alt="PmpPlus"
+          width="150"
+          height="150"
+          style={{ borderRadius: "8px" }}
+        />
+        <Box sx={{ marginLeft: 2, textAlign: "center", width: 500 }}>
+          <Typography
+            variant="h6"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 2,
+            }}
+          >
+            Weather_Condition_App
+          </Typography>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            centered
+            textColor="white"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "orange",
+                color: "white",
+              },
+            }}
+          >
+            <Tab label="LIVE SITE" sx={{ color: "white"  ,cursor: 'pointer'}} onClick={handleTabClick} />
+            <Tab label="GITHUB" sx={{ color: 'white' ,cursor: 'pointer' }} onClick={handleTabClickGithub}/>
+          </Tabs>
+        </Box>
       </Box>
     </Box>
   );
